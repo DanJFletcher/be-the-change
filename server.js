20391b511e10bef3350f2cf37c7f75109167a342
Dan Fletcher <dannyjfletcher@outlook.com>
2016-01-08 09:12:41 -0500

    serve index.html to user

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("hello World!");
+
+/**
+* This server uses Node and ExpressJS to serve a simple home page.
+* Currently, the server is not required for anything in the index.html
+* to function.
+*/
+
+// load the express package and create the app
+var express = require('express'),
+	app     = express(),
+	path = require('path');
+
+// send the index.html file to the user for the home page
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// start the server
+app.listen(8080);
+console.log('There is a hell of a universe on port 8080!');
