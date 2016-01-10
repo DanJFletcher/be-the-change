bd11a832bafdf36652361071e596d39c0dbd181d
Dan Fletcher <dannyjfletcher@outlook.com>
2016-01-09 21:52:42 -0500

    add info: asyncrounous learning model

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// call, apply and bind
+
+var person = {
+	firstname: 'John',
+	lastname: 'Doe',
+	getFullName: function() {
+
+	var fullname = this.firstname + ' ' + this.lastname;
+	return fullname;
+
+	}
+}
+
+var logName = function(lang1, lang2) {
+
+	console.log('Logged: ' + this.getFullName());
+
+}
+
+var logPersonName = logName.bind(person);
+
+logPersonName();
